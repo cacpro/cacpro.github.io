58cda97adc3198ca7af6c419c1ad404e77ba0d90
John Starr <john@cacpro.com>
2020-05-06 22:06:33 -0400

    Updates

--- src/constants.tsx
+++
@@ -1,8 +0,0 @@
-export interface AppConfig {    
-    API_ENDPOINT : string
-}  
-  
-export const CONFIG: AppConfig = {
-    //API_ENDPOINT : "https://web-follow-app.herokuapp.com/api/v1/"
-    API_ENDPOINT : "/"
-};
